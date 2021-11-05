ce14d364aad5796518e616abadd5e2ff3680fdfa
Harry <aghiles.nessah@uha.fr>
2021-11-05 09:44:09 +0100

    fixed chart and added api datas

--- co-2-tracker/src/app/room/room.page.ts
+++ co-2-tracker/src/app/room/room.page.ts
@@ -8,11 +8,11 @@ import { HistoryModalComponent } from '../history-modal/history-modal.component'
 @Component({
   selector: 'app-room',
   templateUrl: 'room.page.html',
-  styleUrls: ['room.page.scss']
+  styleUrls: ['room.page.scss'],
 })
 export class RoomPage {
   @ViewChild('myCanvas') myCanvas: ElementRef;
-  
+
   public context: CanvasRenderingContext2D;
   public date: string = new Date().toISOString();
 
@@ -20,58 +20,104 @@ export class RoomPage {
   roomID: number;
   roomData: any;
   currentModal = null;
-  
+  co2Values = [];
 
-  constructor(private activatedRoute: ActivatedRoute, private modalController : ModalController, private getAPI: GetApiService) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private modalController: ModalController,
+    private getAPI: GetApiService
+  ) {}
 
   async ngOnInit() {
     this.roomID = this.activatedRoute.snapshot.params['id'];
-    await this.getRoomData()
+    await this.getRoomData();
   }
 
   ngAfterViewInit() {
-    this.context = (<HTMLCanvasElement>this.myCanvas.nativeElement).getContext('2d');
-    this.chart = new Chart(<HTMLCanvasElement>this.myCanvas.nativeElement, {
-        type: 'line',
-        data: {
-          labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
-          datasets: [{
-              label: 'Taux de CO2',
-              data: [500, 800, 700, 700, 620, 750, 700, 700, 620, 750],
-              borderColor: 'rgba(0, 0, 255, 1)',
-              borderWidth: 2,
-              tension: 0.1
-          }]
-        },
-        options: {
-          scales: {
-              y: {
-                  min: 400,
-                  max: 1000
-              }
-          }
-      }
-      });
+    setTimeout(() => {
+      this.context = (<HTMLCanvasElement>(
+        this.myCanvas.nativeElement
+      )).getContext('2d');
+      this.chart = new Chart(<HTMLCanvasElement>this.myCanvas.nativeElement, this.chartDatas());
 
-    console.log(this.chart);
+      console.log(this.chart);
+    }, 400);
   }
 
   getRoomData() {
-    console.log(this.roomID)
+    console.log(this.roomID);
     this.getAPI.room(this.roomID).then((response: any) => {
-    this.roomData = response.datas;
-    console.log(this.roomData)
-   });
+      this.roomData = response.datas;
+    });
   }
+
+  chartDatas() : any {
+    var chartData =  {
+      type: 'line',
+      data: {
+          labels: [],
+          datasets: []
+      },
+      options: {
+          scales: {
+            CO2: {
+              type: 'linear',
+              position: 'left',
+            },
+            Webcam: {
+              type: 'linear',
+              position: 'right',
+              ticks: {
+                stepSize: 1
+              }                        
+            }
+          }
+      }
+    }
+
+    var timestamps = [];
+
+    this.roomData.sensors.forEach(sensor => {
+      var chartValues = [];
+
+      var sensorValues = [...sensor.values];
+
+      sensorValues.sort((a, b) => a.timestamp - b.timestamp);
+
+      sensorValues.forEach(value => {
+        if (!timestamps.includes(value.timestamp)) {
+          timestamps.push(value.timestamp);
+        }        
+        chartValues.push(value.value);
+      });
+      chartData.data.datasets.push({
+        label: sensor.type + " - " + sensor.name,
+        data: chartValues,
+        borderColor: this.getRandomColor(),
+        yAxisID: sensor.type,
+        borderWidth: 2,
+        tension: 0.1
+      });
+    });
+
+    chartData.data.labels = timestamps.map(t =>  new Date(t*1000).toLocaleDateString() + ' ' + new Date(t*1000).toLocaleTimeString());
+
+    return chartData;
+  }
+
+
+    getRandomColor(){
+      return 'rgba(' + Math.floor(Math.random() * 255) + ','  + Math.floor(Math.random() * 255) + ','  + Math.floor(Math.random() * 255) + ',1)';
+    }  
 
   async presentModal() {
     const modal = await this.modalController.create({
       component: HistoryModalComponent,
-      componentProps: { homeref: this, roomID: this.roomID},
+      componentProps: { homeref: this, roomID: this.roomID },
       swipeToClose: true,
     });
     this.currentModal = modal;
-    return await modal.present();
+    return await modal.present()
   }
 
   dismissModal() {
@@ -81,5 +127,4 @@ export class RoomPage {
       });
     }
   }
-
 }
